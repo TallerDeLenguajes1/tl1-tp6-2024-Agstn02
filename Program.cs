086e964a85d404ad7c7313f5b33bc3fb025af787
Agustin <agochoc1@gmail.com>
2024-05-14 16:08:42 -0300

    punto 1 terminado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("inversor de numero!");
+        string a;
+        int b;
+        Console.WriteLine("Ingrese un numero:");
+
+        a = Console.ReadLine();
+        char[] aux = a.ToCharArray();
+        Array.Reverse(aux);
+        a = string.Join("",aux);
+        
+        bool prueba = !int.TryParse(a, out b);
+        
+        if(prueba)
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+        
+        Console.WriteLine($"El numero que ingresaste es:{a}. El numero invertido es: {b}");
+        return;
+    }
+}
